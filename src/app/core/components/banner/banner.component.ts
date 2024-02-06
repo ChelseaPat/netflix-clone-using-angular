55408486f969fe06187e60f71d6626b7724896fe
waikato_cp77 <cp77@students.waikato.ac.nz>
2024-02-06 15:33:45 +1300

    Update banner component to show a particular movie

--- src/app/core/components/banner/banner.component.ts
+++ src/app/core/components/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-banner',
@@ -8,6 +9,18 @@ import { Component } from '@angular/core';
     templateUrl: './banner.component.html',
     styleUrl: './banner.component.scss'
 })
-export class BannerComponent {
+export class BannerComponent implements OnChanges {
+    private sanitiser = inject(DomSanitizer);
 
+    @Input({required: true}) title = '';
+    @Input() overview = '';
+    @Input() key = 'lyFFerAxpF0';
+
+    videoUrl = this.sanitiser.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`);
+
+    ngOnChanges ( changes: SimpleChanges ): void {
+        if (changes['key']) {
+            this.videoUrl = this.sanitiser.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`);
+        }
+    }
 }
